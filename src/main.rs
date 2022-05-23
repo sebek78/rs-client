3d06bda1b38a9c566f73f9a634e0029a19ae0cb0
sebek78 <sebasporek@gmail.com>
2022-05-23 21:59:59 +0200

    first GET request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use std::io;
+
+async fn get_data() {
+    let result = reqwest::get("http://localhost:3333/api")
+        .await
+        .unwrap()
+        .text()
+        .await;
+
+    println!("{:?}", result);
+}
+
+#[tokio::main]
+async fn main() {
+    println!("Commands: [ quit, get ]");
+
+    let mut command = String::new();
+    io::stdin()
+        .read_line(&mut command)
+        .expect("Failed to read line");
+    let cmd = command.trim();
+
+    match cmd {
+        "quit" => println!("Q"),
+        "get" => get_data().await,
+        _ => println!("Unknown command: {}", command),
+    }
 }
